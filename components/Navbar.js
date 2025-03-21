e3ac89821aa00d1dc94b22cea840a32020d3ed1f
magicianmk <magicianmk@gmail.com>
2025-03-20 22:20:54 -0700

    logout login

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,29 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useContext } from 'react'
+import AuthContext from '../stores/authContext'
 
+// to use the context, use useContext and AuthContext
+// and access the values passed from the provider
+// as the values update, it will also be updated in the children
 export default function Navbar() {
+  const { user, login, logout } = useContext(AuthContext)
+  console.log(user)
+
   return (
     <div className="container">
       <nav>
-        <Image src="/rupee.png" width={50} height={48} />
+        <Image src="/rupee.png" width={50} height={48} alt='rupee' />
         <h1>Gaming Vibes</h1>
         <ul>
           <li><Link href="/">Home</Link></li>
           <li><Link href="/guides">Guides</Link></li>
+          {!user && <li onClick={login} className='btn'>Login/Signup</li>}
+          {user && <li onClick={logout} className='btn'>Logout</li>}
         </ul>
       </nav>
       <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
+        <Image src="/banner.png" width={966} height={276} alt='banner' />
       </div>
     </div>
   )
---
+++ stores/authContext.js
@@ -0,0 +1,69 @@
+import { createContext, useEffect, useState } from 'react'
+import netlifyIdentity from 'netlify-identity-widget'
+
+// create a context called AuthContext with some default values
+// gives a blueprint on how the context looks 
+const AuthContext = createContext({
+    user: null,
+    login: () => {},
+    logout: () => {},
+    authReady: false
+})
+
+// the context provider AuthContextProvider wraps around the children component in _app.js
+// so that the children have access to the values passing through
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        // set up listener on login event
+        netlifyIdentity.on('login', (user) => {
+            setUser(user)
+            // close the modal
+            netlifyIdentity.close()
+            console.log('login event')
+        })
+
+        // set up listener on logout event
+        netlifyIdentity.on('logout', () => {
+            setUser(null)
+            console.log('logout event')
+        })
+
+        // initalize netlify identity connection
+        netlifyIdentity.init() 
+
+        // unmounts the login and logout listeners
+        // the return functions fire when the AuthContextProvider component unmounts
+        // optional, but good practice 
+        return () => {
+            netlifyIdentity.off('login')
+            netlifyIdentity.off('logout')
+        }
+    }, [])
+
+    // the login function is passed to the Navbar
+    // the netlifyIdentity.open() opens up a pre-made modal
+    // which has a sign up or log in form
+    // the useEffect hook sets up a listener event for login
+    // when login is completed, user object returned from Netlify
+    // will be passed into setUser 
+    // and user will be passed to children
+    const login = () => {
+        netlifyIdentity.open()
+    }
+
+    const logout = () => {
+        netlifyIdentity.logout()
+    }
+
+    const context = { user, login, logout }
+
+    return (
+        <AuthContext.Provider value={context}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext
